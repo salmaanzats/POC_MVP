9d45dc36b7711cbb1de6664c1348df64c942188c
Thanzeel <tnlthanzeel@gmail.com>
2021-02-24 16:00:50 +0530

    update template

---
+++ Templates/edited templates/UpdateCommand/UpdateCommandValidator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace $rootnamespace$.Update$fileinputname$
+{
+    public class $safeitemname$ : AbstractValidator<Update$fileinputname$Command>
+    {
+      public $safeitemname$()
+      {
+
+      }  
+    }
+}
