18f353762c3ed95ceda31bef1885e75128f24b0d
salmaanzats <salmaanzats@gmail.com>
2021-03-08 10:44:06 +0530

    Adding delegates

--- StarGarments.OperationBreakdown/GUI/OperationBreakdown.Designer.cs
+++ StarGarments.OperationBreakdown/GUI/OperationBreakdown.Designer.cs
@@ -39,9 +39,9 @@ namespace StarGarments.OperationBreakdown.GUI
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(-290, -8);
+            this.label1.Location = new System.Drawing.Point(-331, -11);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.Size = new System.Drawing.Size(50, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "label1";
             // 
@@ -49,9 +49,9 @@ namespace StarGarments.OperationBreakdown.GUI
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label2.Location = new System.Drawing.Point(49, 97);
+            this.label2.Location = new System.Drawing.Point(56, 129);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(107, 21);
+            this.label2.Size = new System.Drawing.Size(134, 28);
             this.label2.TabIndex = 1;
             this.label2.Text = "Garment Type";
             // 
@@ -59,40 +59,43 @@ namespace StarGarments.OperationBreakdown.GUI
             // 
             this.cmbGarment_Type.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.cmbGarment_Type.FormattingEnabled = true;
-            this.cmbGarment_Type.Location = new System.Drawing.Point(162, 97);
+            this.cmbGarment_Type.Location = new System.Drawing.Point(185, 129);
+            this.cmbGarment_Type.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.cmbGarment_Type.Name = "cmbGarment_Type";
-            this.cmbGarment_Type.Size = new System.Drawing.Size(404, 29);
+            this.cmbGarment_Type.Size = new System.Drawing.Size(347, 36);
             this.cmbGarment_Type.TabIndex = 2;
             // 
             // cmbStyle
             // 
             this.cmbStyle.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
             this.cmbStyle.FormattingEnabled = true;
-            this.cmbStyle.Location = new System.Drawing.Point(162, 39);
+            this.cmbStyle.Location = new System.Drawing.Point(185, 52);
+            this.cmbStyle.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.cmbStyle.Name = "cmbStyle";
-            this.cmbStyle.Size = new System.Drawing.Size(404, 29);
+            this.cmbStyle.Size = new System.Drawing.Size(347, 36);
             this.cmbStyle.TabIndex = 4;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label3.Location = new System.Drawing.Point(113, 42);
+            this.label3.Location = new System.Drawing.Point(129, 56);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(43, 21);
+            this.label3.Size = new System.Drawing.Size(54, 28);
             this.label3.TabIndex = 3;
             this.label3.Text = "Style";
             // 
             // OperationBreakdownForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(698, 409);
+            this.ClientSize = new System.Drawing.Size(798, 545);
             this.Controls.Add(this.cmbStyle);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.cmbGarment_Type);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "OperationBreakdownForm";
             this.Text = "OperationBreakdown";
             this.ResumeLayout(false);
